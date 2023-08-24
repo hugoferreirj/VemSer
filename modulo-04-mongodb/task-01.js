7315f458cb5cde8573ae6eb0f9059b473953a805
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-08-24 16:23:50 -0300

    correção task 01

--- modulo-04-mongodb/task-01.js
+++ modulo-04-mongodb/task-01.js
@@ -27,3 +27,59 @@ db.alunos.insertMany([
         "data_nascimento" : new Date (2002,07,09)
     }]
 )
+
+-- Operacao 1
+db.alunos.find({"nome": /ia/})
+
+-- saida
+{
+  _id: ObjectId("64e533fea56c871f97cb3df7"),
+  nome: 'Leticia',
+  data_nascimento: 2004-06-27T03:00:00.000Z
+}
+{
+  _id: ObjectId("64e533fea56c871f97cb3df9"),
+  nome: 'Kassia',
+  data_nascimento: 2002-08-09T03:00:00.000Z
+}
+
+-- Operacao 2
+
+db.alunos.find({
+  $expr: {
+    $eq: [{ $year: "$data_nascimento" }, 2002]
+  }
+})
+
+-- saida
+{
+  _id: ObjectId("64e533fea56c871f97cb3df5"),
+  nome: 'Hugo',
+  data_nascimento: 2002-06-09T03:00:00.000Z
+}
+{
+  _id: ObjectId("64e533fea56c871f97cb3df9"),
+  nome: 'Kassia',
+  data_nascimento: 2002-08-09T03:00:00.000Z
+}
+
+
+-- Operacao 3
+
+db.alunos.find({
+  $expr: {
+    $eq: [{ $dayOfMonth: "$data_nascimento" }, 09]
+  }
+})
+
+-- saída
+{
+  _id: ObjectId("64e533fea56c871f97cb3df5"),
+  nome: 'Hugo',
+  data_nascimento: 2002-06-09T03:00:00.000Z
+}
+{
+  _id: ObjectId("64e533fea56c871f97cb3df9"),
+  nome: 'Kassia',
+  data_nascimento: 2002-08-09T03:00:00.000Z
+}
